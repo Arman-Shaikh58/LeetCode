51b9da43a1bd17bf82eef87d8a41e499895cbeca
Arman-Shaikh58 <armanshaikh236058@gmail.com>
2025-12-11 20:32:48 +0530

    did code in Rust

---
+++ Problems/Problem_70/Problem_70.rs
@@ -0,0 +1,25 @@
+use std::io;
+
+fn main(){
+    println!("Hello World");//hehe just for fun
+    println!("Enter a Number");
+    let mut input= String::new();//string because when using 
+    io::stdin().read_line(&mut input).expect("Failed to read Input");
+
+    let n = input.trim().parse().expect("Failed to parse the number");
+
+    let mut step1=1;
+    let mut step2=0;
+    let mut total_ways=0;
+    for _ in 0..n{
+        total_ways=step1+step2;
+        step2=step1;
+        step1=total_ways;
+    }
+
+    println!("The Total ways are: {total_ways}");
+
+
+}
+
+// man i forgot the syntax for for loop and taking input 🥲
