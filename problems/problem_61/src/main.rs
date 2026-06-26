1895f9aad7e2714035eeb4ddb2d7b1ae3b7d3af1
Arman-Shaikh58 <armanshaikh236058@gmail.com>
2026-06-26 13:21:08 +0530

    solved problem 61

---
+++ problems/problem_61/src/main.rs
@@ -0,0 +1,57 @@
+struct Solution;
+
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+impl Solution {
+    pub fn rotate_right(mut head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
+        if head.is_none() || k == 0 {
+            return head;
+        }
+
+        let mut len = 0;
+        let mut curr = &head;
+        while let Some(node) = curr {
+            len += 1;
+            curr = &node.next;
+        }
+
+        let k = (k as usize) % len;
+        if k == 0 {
+            return head;
+        }
+
+        let split_pos = len - k;
+
+        let mut curr = &mut head;
+        for _ in 0..split_pos - 1 {
+            curr = &mut curr.as_mut().unwrap().next;
+        }
+
+        let mut new_head = curr.as_mut().unwrap().next.take();
+
+        let mut tail = &mut new_head;
+        while tail.as_ref().unwrap().next.is_some() {
+            tail = &mut tail.as_mut().unwrap().next;
+        }
+
+        tail.as_mut().unwrap().next = head;
+
+        new_head
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
